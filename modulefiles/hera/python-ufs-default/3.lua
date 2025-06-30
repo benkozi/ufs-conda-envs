d7d068e15219205dcdf596fcdb670fc8dd8df1ee
Ben Koziol <bkoziol@redlineperf.com>
2025-06-30 09:37:44 -0600

    add hera

---
+++ modulefiles/hera/python-ufs-default/3.lua
@@ -0,0 +1,9 @@
+help([[
+  Set environment variables for ufs-conda Python environment
+]])
+
+-- Make sure another version of the same package is not already loaded
+conflict("python-ufs-default")
+
+-- Set environment variables
+prepend_path("PATH","/scratch3/NAGAPE/epic/role-epic/ufs-conda/miniconda3/envs/ufs-default/bin")
