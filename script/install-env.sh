9720c49de5f3f4d3f7c4ab9d8363572787ce4869
Ben Koziol <bkoziol@redlineperf.com>
2025-07-23 11:32:26 -0500

    add jcb pip installation

--- script/install-env.sh
+++ script/install-env.sh
@@ -10,7 +10,6 @@ mkdir -p ${module_dst}
 # Process lua template using sed
 mkdir -p ${_MODULEFILES_INSTALL_DIR}
 sed -e "s|__HELP_DESCRIPTION__|${_HELP_DESCRIPTION}|g" \
-    -e "s|__MODULE_NAME__|${_MODULE_NAME}|g" \
     -e "s|__INSTALL_DIR__|${_INSTALL_DIR}|g" \
     -e "s|__CONDA_ENV_NAME__|${_CONDA_ENV_NAME}|g" \
     ../modulefiles/python-3.lua.template > ${module_dst}/3.lua
