dbc3011f5da1c634c5495a2ea2b09ec563184867
Ben Koziol <bkoziol@redlineperf.com>
2025-07-24 10:30:58 -0500

    added arg to init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,12 @@
 
 set -eux
 
-INSTALL_DIR=/opt/ufs-conda
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <install_directory>"
+  exit 1
+fi
+
+INSTALL_DIR=$1
 
 if [ -e "${INSTALL_DIR}" ]; then
   printf "Error: %s exists. Remove the install directory before proceeding" ${INSTALL_DIR}
