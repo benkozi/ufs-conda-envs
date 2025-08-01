8fd3751661063a33d72450c90ef3da3067df610e
Ben Koziol <bkoziol@redlineperf.com>
2025-08-01 09:30:08 -0500

    try new land-da-workflow

--- spike/test-install-gaea.sh
+++ spike/test-install-gaea.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -xue -o pipefail
 
 export PYTHONPATH=/gpfs/f6/bil-fire8/world-shared/UFS_Land-DA_v3.0/jedi_bundle_sync/build/lib/python3.11
 _CONDABIN=/gpfs/f6/bil-fire8/world-shared/ufs-conda/miniconda3/condabin/conda
@@ -7,3 +8,4 @@ _CONDARUN="${_CONDABIN} run --no-capture-output -n ufs-land-da-wflow-20250801-08
 
 ${_CONDARUN} which python
 ${_CONDARUN} which mpirun
+${_CONDARUN} python -c "from pyioda import ioda"
