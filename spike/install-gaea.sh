c55e6ae72dd0da9463c2d44462f64711236b76cb
Ben Koziol <bkoziol@redlineperf.com>
2025-08-01 09:23:07 -0500

    try new land-da-workflow

--- spike/install-gaea.sh
+++ spike/install-gaea.sh
@@ -3,7 +3,7 @@
 set -xue -o pipefail
 
 _WD=/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/ufs-conda-envs
-_CONDABIN=/gpfs/f6/bil-fire8/world-shared/ufs-conda/condabin/conda
+_CONDABIN=/gpfs/f6/bil-fire8/world-shared/ufs-conda/miniconda3/condabin/conda
 _CONDARUN="${_CONDABIN} run --no-capture-output -n ufs-conda-envs"
 
 cd ${_WD}
