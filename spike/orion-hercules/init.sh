4a25d24aa68e78a9ef4d3aa9fd506090e5ba380e
Ben Koziol <bkoziol@redlineperf.com>
2025-08-01 15:58:12 -0500

    install on orion

--- spike/orion-hercules/init.sh
+++ spike/orion-hercules/init.sh
@@ -2,9 +2,14 @@
 
 set -xue
 
-source ./env.sh
+# Directory to clone ufs-conda-envs to
+_CLONEDIR=/work/noaa/epic/bwkoziol/sandbox/ufs-conda-envs
+# Directory to install conda environments and modulefiles
+_INSTALLDIR=/work/noaa/epic/bwkoziol/tmp/ufs-conda
+
+# -----
 
 git clone -b feat/i9-pin-for-land-da https://github.com/benkozi/ufs-conda-envs.git ${_CLONEDIR}
 cd ${_CLONEDIR}
 
-source ./init.sh ${_INSTALLDIR}
+bash ./init.sh ${_INSTALLDIR}
