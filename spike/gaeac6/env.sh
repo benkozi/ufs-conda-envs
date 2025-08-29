e39a150df967facd6cc6df44614a88865a572205
Ben Koziol <bkoziol@redlineperf.com>
2025-08-29 08:47:01 -0600

    install on gc6 [ci skip]

--- spike/gaeac6/env.sh
+++ spike/gaeac6/env.sh
@@ -6,6 +6,6 @@ _CLONEDIR=/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/ufs-conda-envs
 _BRANCH=feat/i11-add-xesmf
 _CONDABIN=${_INSTALLDIR}/miniconda3/condabin/conda
 _CONDARUN="${_CONDABIN} run --no-capture-output -n ufs-conda-envs"
-_PLATFORM=ursa
+_PLATFORM=gaeac6
 _ENV_KEY=land-da-wflow
 _MODULE_VERSION=i11
