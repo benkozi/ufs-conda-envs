14daba3fb47367232fe1b76eeb5400d6bb845109
Ben Koziol <bkoziol@redlineperf.com>
2025-09-17 10:41:23 -0600

    install on gc6 [ci skip]

--- spike/gaeac6/env.sh
+++ spike/gaeac6/env.sh
@@ -3,9 +3,9 @@
 
 _INSTALLDIR=/gpfs/f6/bil-fire8/world-shared/ufs-conda
 _CLONEDIR=/gpfs/f6/bil-fire8/scratch/Benjamin.Koziol/sandbox/ufs-conda-envs
-_BRANCH=feat/i11-add-xesmf
+_BRANCH=feat/i12-mpas-aerosols
 _CONDABIN=${_INSTALLDIR}/miniconda3/condabin/conda
 _CONDARUN="${_CONDABIN} run --no-capture-output -n ufs-conda-envs"
 _PLATFORM=gaeac6
-_ENV_KEY=land-da-wflow
-_MODULE_VERSION=i11
+_ENV_KEY=mpas-aerosols
+#_MODULE_VERSION=i11
--- spike/gaeac6/install.sh
+++ spike/gaeac6/install.sh
@@ -9,5 +9,6 @@ export PYTHONPATH=${_CLONEDIR}/src
 cd ${_CLONEDIR}
 git pull
 git checkout ${_BRANCH}
+git pull
 ${_CONDARUN} python ${_CLONEDIR}/src/ufs_conda/ufs_conda_cli.py create --help
-${_CONDARUN} python ${_CLONEDIR}/src/ufs_conda/ufs_conda_cli.py create --platform=${_PLATFORM} --env-key=${_ENV_KEY} --module-version=${_MODULE_VERSION}
+${_CONDARUN} python ${_CLONEDIR}/src/ufs_conda/ufs_conda_cli.py create --platform=${_PLATFORM} --env-key=${_ENV_KEY}
