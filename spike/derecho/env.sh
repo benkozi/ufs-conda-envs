3b6bac9b68b94c847d0404cdb5705104dcab7638
Ben Koziol <bkoziol@redlineperf.com>
2026-01-20 09:10:58 -0700

    add uwtools version to da wflow

--- spike/derecho/env.sh
+++ spike/derecho/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-_INSTALLDIR=/glade/work/chanhooj/UFS-DA-ENV/ss19.2/ufs-conda
+_INSTALLDIR=/glade/work/chanhooj/UFS-DA-ENV/ss19.2
 _CLONEDIR=/glade/derecho/scratch/benkoz/sandbox/ufs-conda-envs
 _BRANCH=feat/install-on-derecho
 _CONDABIN=${_INSTALLDIR}/miniconda3/condabin/conda
