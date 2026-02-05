2b4eb8e73afb0d2f82713566700b6ab1cb24cc0b
Ben Koziol <bkoziol@redlineperf.com>
2026-02-05 10:59:14 -0700

    orion work

--- spike/orion-hercules/install.sh
+++ spike/orion-hercules/install.sh
@@ -4,7 +4,7 @@ set -xue
 
 _INSTALLDIR=/work/noaa/epic/UFS-conda
 _CLONEDIR=/work/noaa/epic/bwkoziol/sandbox/ufs-conda-envs
-_BRANCH=feat/i11-add-xesmf
+_BRANCH=fix/broken-env-on-orion
 _CONDABIN=${_INSTALLDIR}/miniconda3/condabin/conda
 _CONDARUN="${_CONDABIN} run --no-capture-output -n ufs-conda-envs"
 
@@ -12,5 +12,5 @@ cd ${_CLONEDIR}
 git pull
 git checkout ${_BRANCH}
 ${_CONDARUN} python ${_CLONEDIR}/src/ufs_conda/ufs_conda_cli.py create --help
-${_CONDARUN} python ${_CLONEDIR}/src/ufs_conda/ufs_conda_cli.py create --platform=orion-hercules --env-key=land-da-wflow --module-version="i11"
-module purge && module use /work/noaa/epic/UFS-conda && module load python-ufs-land-da-wflow-i11
+${_CONDARUN} python ${_CLONEDIR}/src/ufs_conda/ufs_conda_cli.py create --platform=orion-hercules --env-key=land-da-wflow --module-version="20260205-1057"
+module purge && module use /work/noaa/epic/UFS-conda && module load python-ufs-land-da-wflow-20260205-1057
