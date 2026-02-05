0065f76c0b2fb4089251b9c27748dc825b2c2cd1
Ben Koziol <bkoziol@redlineperf.com>
2026-02-05 11:47:50 -0700

    minor

--- spike/orion-hercules/install.sh
+++ spike/orion-hercules/install.sh
@@ -13,4 +13,4 @@ git pull
 git checkout ${_BRANCH}
 ${_CONDARUN} python ${_CLONEDIR}/src/ufs_conda/ufs_conda_cli.py create --help
 ${_CONDARUN} python ${_CLONEDIR}/src/ufs_conda/ufs_conda_cli.py create --platform=orion-hercules --env-key=land-da-wflow --module-version="20260205-1057"
-module purge && module use /work/noaa/epic/UFS-conda && module load python-ufs-land-da-wflow-20260205-1057
+module purge && module use /work/noaa/epic/UFS-conda/modulefiles && module load python-ufs-land-da-wflow-20260205-1057
